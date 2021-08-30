6aaf740fad02aff3182350b8a5e7c962342626de
ZhouKaiDong <zhoukaidong@cangoonline.com>
2021-08-30 10:20:49 +0800

    样式优化

--- blog-admin/jws_modules/hooks/lifeCycle.js
+++ blog-admin/jws_modules/hooks/lifeCycle.js
@@ -27,3 +27,16 @@ export function useForceUpdate() {
     const forceUpdate = useCallback(() => updateState({}), []);
     return forceUpdate;
 }
+
+export function useFetchState(...props) {
+    const focus = useRef();
+    const [state, setState] = useState(...props);
+    useEffect(() => {
+        focus.current = true;
+        return () => (focus.current = false);
+    }, []);
+    const setFetchState = useCallback((...params) => {
+        focus.current && setState(...params);
+    }, []);
+    return [state, setFetchState];
+}
