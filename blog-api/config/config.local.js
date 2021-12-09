fef9ade770639e7f64075584e6d7a8b4e18d7aaa
ZhouKaiDong <zhoukaidong@cangoonline.com>
2021-12-09 09:55:16 +0800

    icon

--- blog-api/config/config.local.js
+++ blog-api/config/config.local.js
@@ -9,16 +9,16 @@ module.exports = appInfo => {
      * @type {Egg.EggAppConfig}
      **/
     const config = exports = {};
-    config.resourceDir = "I:/code/z-blog/resource";
+    config.resourceDir = "I:/code/z-blog-dev/resource";
     config.uploadDir = `${config.resourceDir}/upload`;
     //数据库连接配置
     config.sequelize = {
         dialect: 'mysql', // l类型
-        host: 'rm-j6cp1150r5d5xp0ncqo.mysql.rds.aliyuncs.com', // 地址
-        username: 'zhoukaidong', // 账号
-        password: 'ZkD707396', // 密码
-        port: 9906, // 端口号
-        database: 'zy-blog-dev', // 数据库名称
+        host: '162.14.113.28', // 地址
+        username: 'root', // 账号
+        password: 'ZkD707396/', // 密码
+        port: 3306, // 端口号
+        database: 'z-blog-dev', // 数据库名称
         timezone: '+08:00',//时区
         // 是否自动进行下划线转换（这里是因为DB默认的命名规则是下划线方式，而我们使用的大多数是驼峰方式）
         underscored: true
