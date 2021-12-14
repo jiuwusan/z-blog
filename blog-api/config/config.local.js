22ca1c826d4e404d45f758dbf3a4786b2affe13b
ZhouKaiDong <953343906@qq.com>
2021-12-14 15:32:34 +0800

    配置优化

--- blog-api/config/config.local.js
+++ blog-api/config/config.local.js
@@ -4,19 +4,27 @@
  */
 
 module.exports = appInfo => {
+
+    //获取项目根目录
+    const getRootDir = (dir) => {
+        let subIndex = dir.lastIndexOf("\\");
+        if (subIndex < 0) dir.lastIndexOf("/");
+        return dir.substring(0, subIndex);
+    }
+
     /**
      * built-in config
      * @type {Egg.EggAppConfig}
      **/
     const config = exports = {};
-    config.resourceDir = "I:/code/z-blog-dev/resource";
+    config.resourceDir = `${getRootDir(appInfo.baseDir)}/resource`;
     config.uploadDir = `${config.resourceDir}/upload`;
     //数据库连接配置
     config.sequelize = {
         dialect: 'mysql', // l类型
-        host: '162.14.113.28', // 地址
+        host: '101.43.111.57', // 地址
         username: 'root', // 账号
-        password: 'ZkD707396/', // 密码
+        password: 'ZkD707396', // 密码
         port: 3306, // 端口号
         database: 'z-blog-dev', // 数据库名称
         timezone: '+08:00',//时区
--- blog-api/config/config/redis.js
+++ blog-api/config/config/redis.js
@@ -2,7 +2,7 @@
 const redis = {
     client: {
         port: 6379, // Redis port
-        host: '127.0.0.1', // Redis host
+        host: '101.43.111.57', // Redis host
         password: 'ZkD707396',
         db: 1,
       }
